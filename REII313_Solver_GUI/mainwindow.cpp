dc83e0a0a53540080bd93f58188d45e02e15a1e4
IZAK95 <izak.aden@outlook.com>
2020-06-09 18:15:05 +0200

    Update mainwindow.cpp

--- REII313_Solver_GUI/mainwindow.cpp
+++ REII313_Solver_GUI/mainwindow.cpp
@@ -19,25 +19,47 @@ MainWindow::~MainWindow()
 
 void MainWindow::Setup()
 {
+     //Define Fonts Used
+     QFont DefaultFont("Times",15);
+     QFont TitleFont("Times", 30, QFont::Bold);
+
+     //Set BackGround Colours
+
+     QPalette labelQPalette;
+     labelQPalette.setColor(QPalette::Window,Qt::white);
+
+     //Set Text Colours
+     labelQPalette.setColor(QPalette::WindowText,Qt::blue);
+
     QWidget *widget = new QWidget(this);
     QGridLayout *layout = new QGridLayout(widget);
+
+    //Insert Title
+    QLabel *label = new QLabel("REII313 Prakties",this);
+    label->setMinimumWidth(300);
+    label->setMinimumHeight(100);
+    label->setFont(TitleFont);
+    layout->addWidget(label,0,0);
+
+
     QLabel *labelRowsCount = new QLabel(" Number of limits (rows):", this);
-    layout->addWidget(labelRowsCount, 0, 0);
+    layout->addWidget(labelRowsCount, 1, 0);
 
     QSpinBox *spinRowsCount = new QSpinBox(this); // Number of lines (number of restrictions)
     spinRowsCount->setRange(2, 20);
 
-    layout->addWidget(spinRowsCount, 0, 1);
+    layout->addWidget(spinRowsCount, 1, 1);
 
     QLabel *labelColumnsCount = new QLabel("Number of variables (columns):", this);
-    layout->addWidget(labelColumnsCount, 1, 0);
+    layout->addWidget(labelColumnsCount, 2, 0);
 
     QSpinBox *spinColumnsCount = new QSpinBox(this);
     // Number of columns (number of variables)
     spinColumnsCount->setRange(2, 20);
-    layout->addWidget(spinColumnsCount, 1, 1);
+    layout->addWidget(spinColumnsCount, 2, 1);
 
     QPushButton *buttonContinue = new QPushButton("Next", this);
+   // buttonContinue->setPalette(labelQPalette);
     layout->addWidget(buttonContinue);
     widget->adjustSize();
     widget->move(minimum_X, minimum_Y);
@@ -47,6 +69,7 @@ void MainWindow::Setup()
         widget->hide();
 
         MainWindow::GetVal(spinRowsCount->value(), spinColumnsCount->value());
+        //Grabs values from the UI and sends it to the TableWidget function in order to determine table size.
         int r = spinRowsCount->value();
         int c = spinColumnsCount->value();
         qDebug()<<r<<endl;
@@ -64,12 +87,13 @@ void MainWindow::GetVal(const int &rows, const int &columns)
     // Constraint Matrix
     TableWidget(limitations, rows, columns + 2);
 
+
     for (int i = 0; i < rows; ++i) {
-        QComboBox *cmb = new QComboBox(this);
-        cmb->addItem(" ≥ ");
-        cmb->addItem(" ≤ ");
-        cmb->addItem(" = ");
-        limitations->setCellWidget(i, columns, cmb);
+        QComboBox *combo = new QComboBox(this);
+        combo->addItem(" ≥ ");
+        combo->addItem(" ≤ ");
+        combo->addItem(" = ");
+        limitations->setCellWidget(i, columns, combo);
 
         for (int i = 0; i < columns; ++i)
             limitations->setHorizontalHeaderItem(i, new QTableWidgetItem("x" + QString::number(i + 1)));
@@ -77,8 +101,9 @@ void MainWindow::GetVal(const int &rows, const int &columns)
         limitations->setHorizontalHeaderItem(columns, new QTableWidgetItem("Sign"));
         limitations->setHorizontalHeaderItem(columns + 1, new QTableWidgetItem("Equal"));
         layout->addWidget(limitations, 0, 0);
-
         function = new QTableWidget(this);
+
+
         // objective function
         TableWidget(function, 1, columns + 1);
         QComboBox *extr = new QComboBox(this);
@@ -88,38 +113,58 @@ void MainWindow::GetVal(const int &rows, const int &columns)
         for (int i = 0; i < columns; ++i)
             function->setHorizontalHeaderItem(i, new QTableWidgetItem("x" + QString::number(i + 1)));
 
-        function->setHorizontalHeaderItem(columns, new QTableWidgetItem("Extr"));
+        function->setHorizontalHeaderItem(columns, new QTableWidgetItem("Min/Max"));
         layout->addWidget(function, 1, 0);
 
         QPushButton *buttonContinue = new QPushButton("Solve", this);
-        layout->addWidget(buttonContinue, 2, 0);
+        buttonContinue->setMinimumSize(100,50);
+        layout->addWidget(buttonContinue, 3, 0);
+
+        QPushButton *buttonBack = new QPushButton("Back", this);
+        //buttonBack->setMinimumSize(100,50);
+        layout->addWidget(buttonBack, 2, 0);
+
+
         widget->adjustSize();
-        widget->move(minimum_X, minimum_Y);
+        widget->move(0, 0);
         widget->show();
 
-        connect(buttonContinue, &QPushButton::clicked, this, [=]() {
+        connect(buttonBack, &QPushButton::clicked, this, [=]() {
+            widget->hide();
+            Setup();
+        });
+
+
+
+        connect(buttonContinue, &QPushButton::clicked, this, [=]() {              
             if (!MainWindow::Verify(limitations, function)) {
-                QMessageBox::warning(this, "Message "," Not all fields are filled.");
+
+                QMessageBox *msg = new QMessageBox();
+                msg->warning(this,"Error"," Not all fields are filled.");
+
                 return ;
             }
             widget->hide();
-            // simplex = new QSimplex();
+            // simplex = new Simplex();
             // simplex->initialize(limitations, function);
             // MainWindow::ShowCanonicalLimitationsAndMatrixCoefficients();
         });
     }
 }
 
+//This function draws the table based on the amount of variables and iunputs.
 void MainWindow::TableWidget(QTableWidget *table, const int &rows, const int &columns)
 {
     table->setRowCount(rows);
     table->setColumnCount(columns);
     for (int i = 0; i < columns; ++i)
+    {
         table->setColumnWidth(i, tableColumnWidth);
     table->verticalHeader()->setFixedWidth(tableColumnWidth);
     table->horizontalHeader()->setFixedHeight(tableRowHeight + 5);
     table->setFixedSize(table->horizontalHeader()->length() + table->verticalHeader()->width() + 2,
                         table->verticalHeader()->length() + table->horizontalHeader()->height() + 2);
+    }
 }
 
 bool MainWindow::Verify(const QTableWidget *limitations, const QTableWidget *function) const
